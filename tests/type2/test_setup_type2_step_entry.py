436648eddbc3e461783d877f93d76f5a28e742f1
harrypc <harry261@naver.com>
2026-05-06 23:42:17 +0900

    Update fixed type2 coil settings

--- tests/type2/test_setup_type2_step_entry.py
+++ tests/type2/test_setup_type2_step_entry.py
@@ -13,7 +13,7 @@ _TX_SAMPLED_OWNER_IDS = {"tx_region_actual", "tx_region_actual_stack_space"}
 _TX_REGION_ORIGIN_XYZ = [0.0, -900.0, 0.0]
 _TX_REGION_SIZE_XYZ = [160.0, 1800.0, 90.0]
 _FIXED_TX_REFERENCE_LINE_RATIOS = (0.99, 1.0, 0.9)
-_SAMPLED_TX_REFERENCE_LINE_RANGES = ((0.99, 0.99, 1), (0.2, 1.0, 17), (0.2, 0.9, 13))
+_SAMPLED_TX_REFERENCE_LINE_RANGES = ((0.99, 0.99, 1), (0.2, 1.0, 17), (0.5, 1.0, 13))
 
 
 def _repo_root() -> Path:
